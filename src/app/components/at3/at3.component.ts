6e1516ef156ce0ddc7905aac4b86ee158c598c17
Yurii Storozhuk <yurastorozyk@gmail.com>
2020-10-11 19:10:04 +0300

    planes added

--- src/app/components/at3/at3.component.ts
+++ src/app/components/at3/at3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-at3',
@@ -6,6 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./at3.component.scss']
 })
 export class At3Component implements OnInit {
+
+  public title: string = "Однопілотні однодвигунні поршневі літаки SEP(A)";
+  public image: string = null;
+
+  public info: string = "";
+  public categories: any[] = [{
+    subtitle: 'Технічні характеристики',
+    list: [{
+      name: 'Довжина',
+      value: '6,25 м (20 футів 6 дюймів)'
+    }, {
+        name: 'Розмах крила',
+        value: '7.55 м'
+    },
+    {
+      name: 'Висота',
+      value: '2,23 м (7 футів 4 дюймів)'
+    },
+    {
+      name: 'Площа крила',
+      value: '9,3 м ² (100,6 кв.м)'
+    },
+    {
+      name: 'Маса пустого літака',
+      value: '350 кг (771 фунтів)'
+    },
+    {
+      name: 'Корисне навантаження',
+      value: '232 кг (511 фунтів)'
+    },
+    {
+      name: 'Максимальна злітна маса',
+      value: '582 кг (1,282фунтів)'
+    },
+    {
+      name: 'Двигун',
+      value: '1 × Rotax 912 S Elprop 3-1-1P, 100 к.с. (75 кВт)'
+    },
+    {
+      name: 'Максимальна швидкість',
+      value: '220 км/год (119 вузлів)'
+    },
+    {
+      name: 'Крейсерська швидкість',
+      value: '200 км/год (108 вузлів)'
+    },
+    {
+      name: 'Швидкість звалювания',
+      value: '82 км/год (44 вузлів)'
+    },
+    {
+      name: 'Дальність польоту',
+      value: '717 км (387 миль)'
+    },
+    {
+      name: 'Практична стеля',
+      value: '4000 м (13123 футів)'
+    },
+    {
+      name: 'Злітна дистанція',
+      value: '145 м (476 футів)'
+    },
+    {
+      name: 'Посадкова дистанція',
+      value: '200 м (656 футів)'
+    },
+    ],
+  }]
 
   constructor() { }
 
--- src/app/components/ir/ir.component.ts
+++ src/app/components/ir/ir.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IRComponent implements OnInit {
 
+  public title: string = "Курс підготовки для отримання рейтингу IR (право виконання польотів за приладами)";
+  public image: string = null;
+
+  public info: string = "Призначенням курсу є підготовка для отримання рейтингу IR на однопілотному багатодвигуннму літаку (земля) (МЕР(L).";
+  public categories: any[] = [{
+    subtitle: 'Курс складається з:',
+    list: [{
+      name: 'Практична тренувальна програма',
+      value: ''
+    }, {
+      name: 'Екзаменаційний політ',
+        value: '1 година  30 хвилин'
+    }],
+     description: 'Розрахунковий час проходження курсу – два дні, з нальотом:'
+  }, {
+    subtitle: 'Вимоги до кандидатів:'
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
